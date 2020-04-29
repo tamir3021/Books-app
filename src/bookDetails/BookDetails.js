87b8cf6480baf26ece7a0150ef1846704dd7fc6f
Tamir Feldman <tamir@shieldfc.com>
2020-04-29 17:17:40 +0300

    Added first HTTP request to google Books API.

--- src/bookDetails/BookDetails.js
+++ src/bookDetails/BookDetails.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './BookDetails'
 
-function BookDetails() {
-    return (
-        <aside className="bookDetails">
-            <p>Book details</p>
-        </aside>
-    )
+class BookDetails extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        fetch("https://www.googleapis.com/books/v1/volumes?q=thestand")
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    }
+
+    render() {
+        return (
+            <aside className="bookDetails">
+                <p>Book details</p>
+            </aside>
+        )
+    }
 }
 
 export default BookDetails;
